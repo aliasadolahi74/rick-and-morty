f5d77c95aa3aac3e40e9a113b94644d6c36b8894
Ali Asadollahi <ali.asadolahi74@gmail.com>
2023-04-04 12:09:57 +0330

    characters added to index page

--- src/routes/Index/Index.jsx
+++ src/routes/Index/Index.jsx
@@ -1,14 +1,26 @@
 import React, { useState, useEffect } from "react";
-import dummy from "../../dummy/dummy.json";
+import data from "../../dummy/dummy.json";
 import Header from "../../components/Header/Header";
+import classes from "./Index.module.css";
+import Character from "../../components/Character/Character";
 
 const Index = () => {
-  console.log("dummy", dummy);
-  const [data, setData] = useState();
+  // console.log("dummy", data);
+  // const [data, setData] = useState();
+
+  const { result: characters, info } = data;
+  const { next, prev, pages } = info;
 
   return (
-    <div>
+    <div className={classes.Container}>
       <Header />
+      <div className={classes.Content}>
+        <div className={classes.Characters}>
+          {characters?.map((character) => (
+            <Character key={character.id} info={character} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
