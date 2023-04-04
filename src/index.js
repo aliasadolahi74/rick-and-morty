6e5fbac02d3bc8107b3404a01b6bf0bcc03de89b
Ali Asadollahi <ali.asadolahi74@gmail.com>
2023-04-04 11:41:36 +0330

    path error resolved

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Index from "./Routes/Index/Index";
+import Index from "./routes/Index/Index";
 
 const router = createBrowserRouter([
   {
