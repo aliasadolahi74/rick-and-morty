0e942d451876d4a07d73f8cf7a0b0e456b8e2d8b
Ali Asadollahi <ali.asadolahi74@gmail.com>
2023-04-04 11:39:39 +0330

    routes added

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Index from "./Routes/Index/Index";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Index />,
   },
 ]);
 
