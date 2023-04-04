821e478f93f3390cbc895221122f12e0c201335a
Ali Asadollahi <ali.asadolahi74@gmail.com>
2023-04-04 11:52:42 +0330

    index page added

--- src/routes/Index/Index.jsx
+++ src/routes/Index/Index.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import dummy from "../../dummy/dummy.json";
 
 const Index = () => {
+  console.log("dummy", dummy);
+  const [data, setData] = useState();
+
   return <div>Index page</div>;
 };
 
