3e574f5bfd8ab90cd62cdf739cb1ba49fbf8c636
Ali Asadollahi <ali.asadolahi74@gmail.com>
2023-04-04 11:57:08 +0330

    header completed

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={classes.Container}>
+      <h1>Rick and Morty Characters</h1>
+    </div>
+  );
+};
+
+export default Header;
--- src/routes/Index/Index.jsx
+++ src/routes/Index/Index.jsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
 import dummy from "../../dummy/dummy.json";
+import Header from "../../components/Header/Header";
 
 const Index = () => {
   console.log("dummy", dummy);
   const [data, setData] = useState();
 
-  return <div>Index page</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Index;
